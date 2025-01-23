6efdfdc4e67c8c48d82887ba91df4d097440d261
wan lijun <lietcooper123@gmail.com>
2025-01-22 22:54:53 -0800

    revised comment style

--- src/main/java/student/Greeting.java
+++ src/main/java/student/Greeting.java
@@ -1,33 +1,42 @@
 package student;
 
 /**
- * This class is a place holder which you will fully implement based on the javadoc
- * 
+ * This class is a place holder which you will fully implement based on the javadoc.
+ *
  * https://cs5004-khoury-lionelle.github.io/hello_world/student/package-summary.html
- * 
+ *
  */
 public class Greeting {
+    /** int value of locality ID. */
     private final int localityID;
+
+    /** String value of the locality name. */
     private final String localityName;
+
+    /** String value of the greeting in ascii characters. */
     private final String asciiGreeting;
+
+    /** String value of the greeting in unicode characters. */
     private final String unicodeGreeting;
+
+    /** String value of the format string. */
     private final String formatStr;
 
     /**
-     * This constructor creates default greeting "Hello, {name}!"
-     * @param localityID, id of the locality
-     * @param localityName, name of the locality
+     * This constructor creates default greeting "Hello, {name}!".
+     * @param localityID id of the locality
+     * @param localityName name of the locality
      */
     public Greeting(int localityID, String localityName) {
         this(localityID, localityName, "Hello");
     }
 
     /**
-     * This constructor creates a greeting with ascii and Unicode characters
+     * This constructor creates a greeting with ascii and Unicode characters.
      * assuming the language is already using ascii letters only.
-     * @param localityID, id of the locality
-     * @param localityName, name of the locality
-     * @param greeting, greeting using ascii characters
+     * @param localityID id of the locality
+     * @param localityName name of the locality
+     * @param greeting greeting using ascii characters
      */
     public Greeting(int localityID, String localityName, String greeting) {
         this(localityID, localityName, greeting, greeting, "%s, %%s!");
@@ -35,13 +44,14 @@ public class Greeting {
 
     /**
      * This constructor creates a greeting with ascii and Unicode characters.
-     * @param localityID, id of the locality
-     * @param localityName, name of the locality
-     * @param asciiGreeting, greeting using ascii characters
-     * @param unicodeGreeting, greeting using Unicode characters
-     * @param formatStr, format string for the greeting
+     * @param localityID id of the locality
+     * @param localityName name of the locality
+     * @param asciiGreeting greeting using ascii characters
+     * @param unicodeGreeting greeting using Unicode characters
+     * @param formatStr format string for the greeting
      */
-    public Greeting(int localityID, String localityName, String asciiGreeting, String unicodeGreeting, String formatStr) {
+    public Greeting(int localityID, String localityName, String asciiGreeting,
+                    String unicodeGreeting, String formatStr) {
         this.localityID = localityID;
         this.localityName = localityName;
         this.asciiGreeting = asciiGreeting;
@@ -50,15 +60,15 @@ public class Greeting {
     }
 
     /**
-     * Return the locality id number
-     * @return the int value of locality id number
+     * Return the locality id number.
+     * @return localityID the int value of locality id number
      */
     public int getLocalityID() {
         return localityID;
     }
 
     /**
-     *
+     * Return the name of the locality.
      * @return the locality name
      */
     public String getLocalityName() {
@@ -66,7 +76,7 @@ public class Greeting {
     }
 
     /**
-     *
+     * Return the greeting using ascii characters.
      * @return the greeting using ascii characters
      */
     public String getAsciiGreeting() {
@@ -74,15 +84,15 @@ public class Greeting {
     }
 
     /**
-     *
-     * @return the greeting using Unicode characters
+     * Return the greeting using Unicode characters.
+     * @return unicodeGreeting. the greeting using Unicode characters
      */
     public String getUnicodeGreeting() {
         return unicodeGreeting;
     }
 
     /**
-     *
+     * Return the format steing with greeting inserted into the format.
      * @return the format string with greeting inserted into the format
      */
     public String getFormatStr() {
@@ -90,16 +100,16 @@ public class Greeting {
     }
 
     /**
-     *
-     * @param asciiOnly, if true, only ascii characters will be used
+     * Return the format string with the greeting inserted into the string.
+     * @param asciiOnly if true, only ascii characters will be used
      * @return the format string with the greeting inserted into the string
      */
     public String getFormatStr(boolean asciiOnly) {
-        return asciiOnly? String.format(formatStr, asciiGreeting) : String.format(formatStr, unicodeGreeting);
+        return asciiOnly ? String.format(formatStr, asciiGreeting) : String.format(formatStr, unicodeGreeting);
     }
 
     /**
-     *
+     * Return the string representation of the object.
      * @return the full greeting details as a String. Primarily for debugging purposes.
      */
     public String toString() {
